b802ad0861074d7d4d7453f7edb8ee8c7369f6a0
James Chege <jymeschege@gmail.com>
2021-01-22 01:25:21 +0300

    ch(test): fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,14 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Register Courses/i);
   expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Welcome, hope you\'ve had a great time!')).toBeInTheDocument();
+});
+
+test('redirects if user is already logged in', () => {
+  window.localStorage.setItem('token', 'tokenstring');
+  render(<App />);
+  expect(screen.getByText(/Search student here/i)).toBeInTheDocument();
+  window.localStorage.removeItem('token');
 });
